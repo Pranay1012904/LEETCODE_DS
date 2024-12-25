b1ce944f8c6cd09957f9e63211be702613485e08
unknown <pranay.dmnsns@gmail.com>
2024-12-26 00:15:53 +0530

    added two more problems

---
+++ src/Operation/StringOps/RomanToInteger.java
@@ -0,0 +1,50 @@
+package Operation.StringOps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+
+    static public void main(String[] args){
+        String rm="MCMXCIV";
+        int i=1;
+        int sum=0;
+
+        //Remember cfalling the function all the time eg:dictionary().get.... consumes lots of time
+        if(rm.length()==1)
+            sum+=dictionary().get(String.valueOf(rm.charAt(0)));
+        else{
+            while(i<=rm.length()){
+                if(i==rm.length()) {
+                    sum += dictionary().get(String.valueOf(rm.charAt(i - 1)));
+                    i++;
+                }
+                else if(dictionary().get(String.valueOf(rm.charAt(i-1)))> dictionary().get(String.valueOf(rm.charAt(i)))){
+                     sum+=dictionary().get(String.valueOf(rm.charAt(i-1)));
+                      i++;
+                }
+                else if(dictionary().get(String.valueOf(rm.charAt(i-1)))< dictionary().get(String.valueOf(rm.charAt(i)))){
+                        sum+=dictionary().get(String.valueOf(rm.charAt(i)))-dictionary().get(String.valueOf(rm.charAt(i-1)));
+                    i+=2;
+                }else if(dictionary().get(String.valueOf(rm.charAt(i-1))) == dictionary().get(String.valueOf(rm.charAt(i)))){
+                    sum+=dictionary().get(String.valueOf(rm.charAt(i-1)));
+                    i++;
+                }
+            }
+        }
+        System.out.print("\n\nSUM::"+sum);
+    }
+
+    public static Map<String, Integer> dictionary(){
+        Map<String, Integer> dictionary=new HashMap<>();
+        dictionary.put("I",1);
+        dictionary.put("V",5);
+        dictionary.put("X",10);
+        dictionary.put("L",50);
+        dictionary.put("C",100);
+        dictionary.put("D",500);
+        dictionary.put("M",1000);
+        return dictionary;
+    }
+
+}
---
+++ src/Operation/StringOps/ValidParathesis.java
@@ -0,0 +1,46 @@
+package Operation.StringOps;
+
+import java.util.*;
+
+public class ValidParathesis {
+
+    public static void main(String[] args) {
+        validity();
+    }
+
+    public static void validity() {
+        String regex = "[]{}()";
+
+        List<String> regList = Arrays.asList(regex.split(""));//note it down
+        int size = regList.size();
+        boolean flag = true;
+        if(regList.size()%2!=0 || (regList.get(0).equals(")") || regList.get(0).equals("}") || regList.get(0).equals("]")))
+            flag= false;
+        Stack<String> stack = new Stack<>();
+        int i = 0;
+
+        while (flag && i < size) {
+            if (
+                    (regList.get(i).equals("(") ||
+                            regList.get(i).equals("{") ||
+                            regList.get(i).equals("["))
+            ) {
+                stack.push(regList.get(i));
+                i++;
+            } else if ( !stack.isEmpty() &&
+                    (regList.get(i).equals(")") && stack.get(stack.size() - 1).equals("(") ||
+                            regList.get(i).equals("}") && stack.get(stack.size() - 1).equals("{") ||
+                            regList.get(i).equals("]") && stack.get(stack.size() - 1).equals("["))
+            ) {
+                stack.pop();
+                i++;
+            } else
+                flag = false;
+        }
+
+        if(!stack.isEmpty() || !flag)
+            flag= false;
+
+        System.out.print(flag);
+    }
+}
