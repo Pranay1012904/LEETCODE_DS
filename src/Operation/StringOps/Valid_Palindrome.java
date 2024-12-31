6385d10ca0908afce536b8a89acc57dbcc4095ee
Pranay1012904 <pranay.dmnsns@gmail.com>
2025-01-01 02:56:06 +0530

    Added valid palindrome prob

--- src/Operation/StringOps/Valid_Palindrome.java
+++ src/Operation/StringOps/Valid_Palindrome.java
@@ -1,4 +1,26 @@
 package Operation.StringOps;
+/*
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ */
 
 public class Valid_Palindrome {
 
@@ -13,8 +35,8 @@ public class Valid_Palindrome {
         }
            String sbSt=sb.toString();
            String sbRev=sb.reverse().toString();
-           //be careful while using StringBuilder. If you reverse it sb.reverse.
-        // It's value will get reverserd everywhere!
+           //be careful while using StringBuilder. If you reverse it sb.reverse().
+        // It's value will get reverserd everywhere! sb itself will get reverserd, it wont return a reversed value
         System.out.print(sbRev.equals(sbSt));
     }
 }
